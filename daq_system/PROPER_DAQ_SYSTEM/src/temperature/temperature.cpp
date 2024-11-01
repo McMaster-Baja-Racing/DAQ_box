9a8f96638756f5398758e1a449de2a1208beee96
t-wing11 <Traviswing20@gmail.com>
2024-10-31 23:03:48 -0400

    merge conflict

--- daq_system/PROPER_DAQ_SYSTEM/src/temperature/temperature.cpp
+++ daq_system/PROPER_DAQ_SYSTEM/src/temperature/temperature.cpp
@@ -1,14 +1,31 @@
 #include "temperature.h"
 #include "../sdCard/sdCard.h"
 #include "../datastruct/dataTypes.h"
+#include <Adafruit_MCP9600.h>
 
 // Initializations
-int temperature;
+float temperature;
+bool mcp_initialized = false;
+Adafruit_MCP9600 mcp;
 
 // Function Definitions
 void tempData() {
-  temperature = (analogRead(TEMPERATURE_PIN))/*/2*/;
-  //  Serial.print("primtemp: ");
-  //  Serial.println(temperature);
-  buffPush(PRIM_TEMP, (unsigned long)(temperature));
+  //temperature = (analogRead(TEMPERATURE_PIN))/*/2*/;
+  //Serial.print("primtemp: ");
+  //Serial.println(temperature);
+  if(mcp_initialized) {
+    temperature = mcp.readThermocouple();
+    buffPush(PRIM_TEMP, (unsigned long)(temperature));
+  }
+}
+
+void tempSetup() {
+  if (!mcp.begin(0x67)) {
+    Serial.println("MCP9600 failed, or not present");
+    mcp_initialized = false;
+  } else {
+    mcp.setThermocoupleType(MCP9600_TYPE_K);
+    Serial.println("MCP9600 K-TYPE SUCCESS");
+    mcp_initialized = true;
+  }
 }
