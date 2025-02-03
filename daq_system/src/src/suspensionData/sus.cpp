2cdb7254e0fdee2749d321ffa887c5eaa26cc930
t-wing11 <Traviswing20@gmail.com>
2025-02-03 17:15:11 -0500

    Updates to sus and sheave position

--- daq_system/src/src/suspensionData/sus.cpp
+++ daq_system/src/src/suspensionData/sus.cpp
@@ -3,10 +3,10 @@
 #include "../datastruct/dataTypes.h"
 
 // Initializations
-float mRight = -24.282;  // Slope for the right sensor
-float bRight = 439.54;  // Intercept for the right sensor
-float mLeft = -16.843;   // Slope for the left sensor
-float bLeft = 406.62;   // Intercept for the left sensor
+float mRight = 0.8812;  // Slope for the right sensor
+float bRight = -2.66;  // Intercept for the right sensor
+float mLeft = 1.091;   // Slope for the left sensor
+float bLeft = -4.13;   // Intercept for the left sensor
 
 int sus1;
 int sus2;
@@ -14,27 +14,27 @@ int sus3;
 int sus4;
 
 // Function Definitions
-void susData1() { // Front Left
-  int sensorValueRight = analogRead(susPin[0]);
-  float suspensionDistanceRight = ( sensorValueRight - bRight) / mRight;
-  //Serial.print("Suspension Distance Right: ");
-  //Serial.println(suspensionDistanceRight);
-  buffPush(SUS_TRAV_FR, (unsigned long)(suspensionDistanceRight));
+void susData1() { // back Left
+  int sensorValueLeft = analogRead(susPin[0]);
+  //Serial.print("Sensor Value Left: ");
+  //Serial.println(sensorValueLeft);
+  //float suspensionDistanceLeft = ( sensorValueLeft - bLeft) / mLeft;
+  buffPush(SUS_TRAV_RL, (unsigned long)(sensorValueLeft));
 }
 
-void susData2() { // Front Right
-  int sensorValueLeft = analogRead(susPin[1]);
-  float suspensionDistanceLeft = ( sensorValueLeft - bLeft) / mLeft;
-  //Serial.print("Suspension Distance Left: ");
-  //Serial.println(suspensionDistanceLeft);
-  buffPush(SUS_TRAV_FL, (unsigned long)(suspensionDistanceLeft)); 
+void susData2() { // back right
+  int sensorValueRight = analogRead(susPin[1]);
+  //Serial.print("Sensor Value right: ");
+  //Serial.println(sensorValueRight);
+  //float suspensionDistanceRight = ( sensorValueRight - bRight) / mRight;
+  buffPush(SUS_TRAV_RR, (unsigned long)(sensorValueRight)); 
 }
 
 void susData3() { // Rear Right
   sus3 = analogRead(susPin[2]);
   //  Serial.print("susdata3: ");
   //  Serial.println(sus3);
-  buffPush(SUS_TRAV_RR, (unsigned long)(sus3));
+  //buffPush(SUS_TRAV_RR, (unsigned long)(sus3));
 }
 
 void susData4() { // This is the data for the steering column
